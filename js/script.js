4032b68d3416e3e5aa4c83bc481ee3c39a673350
moonpita <75728982+moonpita@users.noreply.github.com>
2021-04-05 23:18:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const element = document.querySelector('#selectCustom');
+//          const choices = new Choices(element, {
+//              searchEnabled: true
+//          });
+// const element = document.querySelector('#selectCustom2');
+//          const choices = new Choices(element, {
+//              searchEnabled: true
+//          });
+
+// const element = document.querySelector('#selectCustom3');
+//          const choices = new Choices(element, {
+//              searchEnabled: true
+//          });
